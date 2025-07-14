dcdc79cdbf4d25444a5cb33fb765fcb42236c162
Saltine <156854448+saltytine@users.noreply.github.com>
2025-07-14 18:04:53 -0400

    more yap

--- src/start.s
+++ src/start.s
@@ -4,6 +4,7 @@
 .section ".text.boot"
 
 _start:
-    ldr x30 , =stack_top //; stack top pointer
-    mov sp , x30 //; transfer
-    bl not_main //; jump
+    ldr  x30     , =stack_top //; stack top pointer
+    mov  sp      , x30        //; transfer
+    bl   not_main             //; jump
+    b    .                    //; it will never run here
