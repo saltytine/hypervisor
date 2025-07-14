a17524d7f440c5096c5be8fe3f5bb3afe2fa9dbf
Saltine <156854448+saltytine@users.noreply.github.com>
2025-07-14 17:49:23 -0400

    fix compiler error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("hello world");
+#![no_std] // rm standard library linking
+#![no_main] // use the actual entry_start, not the main entry
+
+use core::arch::global_asm;
+use core::ptr; // inline asm support
+
+mod panic;
+
+global_asm!(include_str!("start.s")); // inline asm
+
+// #[lang = "eh_personality"]
+#[no_mangle] // make it so rust doesnt modify the function name we define during compilation
+pub extern "C" fn not_main() {
+    // see: https://en.wikipedia.org/wiki/Calling_convention
+    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    let out_str = b"AARCH Bare Metal";
+    for byte in out_str {
+        unsafe {
+            ptr::write_volatile(UART0, *byte);
+        }
+    }
 }
---
+++ src/start.rs
@@ -0,0 +1,9 @@
+.global _start
+.extern stack_top
+
+.section ".text.boot"
+
+_start:
+    ldr x30 , =stack_top //; stack top pointer
+    mov sp , x30 //; transfer
+    bl not_main //; jump
