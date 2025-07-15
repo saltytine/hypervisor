735827f668178e03bc7c14064f13d3bd75da09be
Saltine <156854448+saltytine@users.noreply.github.com>
2025-07-14 20:26:10 -0400

    boot args with el2/3

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,44 @@ global_asm!(include_str!("start.s")); // inline asm
 pub extern "C" fn not_main() {
     // see: https://en.wikipedia.org/wiki/Calling_convention
     const UART0: *mut u8 = 0x0900_0000 as *mut u8;
-    let out_str = b"AARCH Bare Metal";
+    let out_str = b"AARCH Bare Metal\n";
     for byte in out_str {
         unsafe {
             ptr::write_volatile(UART0, *byte);
         }
     }
+}
+#[no_mangle]
+pub extern "C" fn el3_entry() -> u8 {
+    printk_uart0("this is el3_entry.........\n")
+}
+
+#[no_mangle]
+pub extern "C" fn el2_entry() -> u8 {
+    printk_uart0("This is el2_entry.........\n")
+}
+
+fn printk_uart0(str: &str) -> u8 {
+    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    for byte in str.bytes() {
+        unsafe {
+            ptr::write_volatile(UART0, byte);
+        }
+    }
+    return 0;
+}
+
+pub fn boot_hypervisor() -> u8 {
+    printk_uart0("Hello Hypervisor\n");
+    /*
+     * 1. configure related registers;
+     * 2. configure page table information;
+     * 3. other configurations;
+     * 4. vcpu_init;
+     * 5. ram_init;
+     * 6. irq_init;
+     * 7. load_image;
+     * 8. vcpu_run;
+     */
+    return 0;
 }
--- src/start.s
+++ src/start.s
@@ -4,7 +4,14 @@
 .section ".text.boot"
 
 _start:
-    ldr  x30     , =stack_top //; stack top pointer
-    mov  sp      , x30        //; transfer
-    bl   not_main             //; jump
-    b    .                    //; it will never run here
+    ldr   x30       , =stack_top      //; stack top pointer
+    mov   sp        , x30             //; transfer
+    mrs   x5        , CurrentEL       //; current exception level moves to x5
+    ubfx  x5, x5, #2, #2
+    cmp   x5        , 3         //; check if its el3
+    b.eq  el3_entry                   //; if yes，enter el3_entry
+    b     el2_entry                   //; otherwise，enter el2_entry
+
+
+    bl    not_main                    //; jump
+    b     .                           //; never run here
