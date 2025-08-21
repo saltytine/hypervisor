e02ddc86eaaba25a890f952fd82a1083fd00faa7
Saltine <156854448+saltytine@users.noreply.github.com>
2025-08-21 12:13:47 -0400

    enable sgi to wake up cpus

--- src/device/irqchip/gicv3/gicr.rs
+++ src/device/irqchip/gicv3/gicr.rs
@@ -4,9 +4,14 @@
 
 //! GICC Driver - GIC CPU interface.
 
-use super::gicd::{
-    GICD_ICACTIVER, GICD_ICENABLER, GICD_ICFGR, GICD_ICPENDR, GICD_IGROUPR, GICD_IPRIORITYR,
-    GICD_ISACTIVER, GICD_ISENABLER, GICD_ISPENDR,
+use crate::{arch::cpu::this_cpu_data, hypercall::SGI_EVENT_ID};
+
+use super::{
+    gicd::{
+        GICD_ICACTIVER, GICD_ICENABLER, GICD_ICFGR, GICD_ICPENDR, GICD_IGROUPR, GICD_IPRIORITYR,
+        GICD_ISACTIVER, GICD_ISENABLER, GICD_ISPENDR,
+    },
+    host_gicr_base,
 };
 
 pub const GICR_CTLR: usize = 0x0000;
@@ -29,7 +34,6 @@ pub const GICR_IPRIORITYR: usize = GICD_IPRIORITYR;
 pub const GICR_ICFGR: usize = GICD_ICFGR;
 pub const GICR_TYPER_LAST: usize = 1 << 4;
 
-#[allow(unused)]
 pub fn enable_ipi() {
     todo!();
     // let base = this_cpu_data().gicr_base + GICR_SGI_BASE;
@@ -55,4 +59,28 @@ pub fn enable_ipi() {
     //     let gicr_waker = (base + GICR_WAKER) as *mut u32;
     //     gicr_waker.write_volatile(gicr_waker.read_volatile() & !0x02);
     // }
+    let base = host_gicr_base(this_cpu_data()) + GICR_SGI_BASE;
+
+    unsafe {
+        let gicr_waker = (base + GICR_WAKER) as *mut u32;
+        gicr_waker.write_volatile(gicr_waker.read_volatile() & !0x02);
+        while gicr_waker.read_volatile() & 0x04 != 0 {}
+
+        let gicr_igroupr0 = (base + GICR_IGROUPR) as *mut u32;
+        gicr_igroupr0.write_volatile(gicr_igroupr0.read_volatile() | (1 << SGI_IPI_ID));
+
+        let gicr_isenabler0 = (base + GICR_ISENABLER) as *mut u32;
+        gicr_isenabler0.write_volatile(1 << SGI_IPI_ID);
+
+        let gicr_ipriorityr0 = (base + GICR_IPRIORITYR) as *mut u32;
+        {
+            let reg = SGI_IPI_ID / 4;
+            let offset = SGI_IPI_ID % 4 * 8;
+            let mask = ((1 << 8) - 1) << offset;
+            let p = gicr_ipriorityr0.add(reg as _);
+            let prio = p.read_volatile();
+
+            p.write_volatile((prio & !mask) | (0x01 << offset));
+        }
+    }
 }
